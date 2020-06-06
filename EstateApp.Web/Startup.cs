f05753ed4ee247d2577be90c8b600d881a81a4cd
agharayee <agharayee@gmail.com>
2020-06-06 01:50:07 +0100

    added Ef Database

--- EstateApp.Web/Startup.cs
+++ EstateApp.Web/Startup.cs
@@ -2,14 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EstateApp.Data.DatabaseContext.AuthenticationDbContext;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace EstateApp
+namespace EstateApp.Web
 {
     public class Startup
     {
@@ -23,6 +25,7 @@ namespace EstateApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AuthenticationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
         }
 
